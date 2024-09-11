a7fe9950c106a61778d5b9286d737d05581588f1
Leo Jacquet <leojacquet2003@gmail.com>
2024-09-11 21:47:34 +0200

    🔧 configure app

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
 import react from '@vitejs/plugin-react-swc';
+import { fileURLToPath } from 'url';
 import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react()]
+    plugins: [react()],
+    base: '/rest-countries-api-with-color-theme-switcher',
+    resolve: {
+        alias: {
+            '@': fileURLToPath(new URL('./src', import.meta.url))
+        }
+    }
 });
